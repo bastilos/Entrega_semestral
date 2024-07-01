3f63e0e6dc04b48bf3bb858c1b986bb5712cd486
bastilos <bas.cabezas@duocuc.cl>
2024-07-01 00:09:52 -0400

    incorporando codigo

--- thegameson/core/models.py
+++ thegameson/core/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from core.templatetags.custom_filters import formatear_dinero
 from django.db import models
 from django.db.models import Min
 from django.db import connection
